ce9f6bdaf48bef3323dca01c9eb7110379309341
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2023-12-25 19:16:00 +0530

    can add text

--- main.js
+++ main.js
@@ -1,29 +1,17 @@
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
-
 var video = document.querySelector("video");
 
 var isDrawing = false;
 var lastX = 0;
 var lastY = 0;
+var inputText = "";
+var clickX = 0;
+var clickY = 0;
+var mode = "draw"; // Initial mode is drawing
 
-// var colors = ["red", "blue", "yellow", "orange", "black", "white", "green"];
-// function draw() {
-//   ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
-//   ctx.fillRect(0, 0, canvas.width, canvas.height);
-// }
-// draw();
-
-canvas.addEventListener("mousedown", function (event) {
-  isDrawing = true;
-  [lastX, lastY] = [
-    event.clientX - canvas.offsetLeft,
-    event.clientY - canvas.offsetTop,
-  ];
-});
-
-canvas.addEventListener("mousemove", function (event) {
-  if (isDrawing) {
+function draw(event) {
+  if (mode === "draw" && isDrawing) {
     const x = event.clientX - canvas.offsetLeft;
     const y = event.clientY - canvas.offsetTop;
 
@@ -34,10 +22,57 @@ canvas.addEventListener("mousemove", function (event) {
 
     [lastX, lastY] = [x, y];
   }
+}
+
+function handleTextInput(event) {
+  if (mode === "text") {
+    if (event.key === "Enter") {
+      mode = "draw"; // Switch back to drawing mode on pressing Enter
+    } else if (event.key === "Backspace") {
+      inputText = inputText.slice(0, -1);
+    } else {
+      inputText += event.key;
+    }
+
+    drawText();
+  }
+}
+
+function drawText() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.font = "48px serif";
+  ctx.fillStyle = "black";
+  ctx.fillText(inputText, clickX, clickY);
+}
+
+canvas.addEventListener("mousedown", function (event) {
+  if (mode === "draw") {
+    isDrawing = true;
+    [lastX, lastY] = [
+      event.clientX - canvas.offsetLeft,
+      event.clientY - canvas.offsetTop,
+    ];
+  } else if (mode === "text") {
+    clickX = event.clientX - canvas.offsetLeft;
+    clickY = event.clientY - canvas.offsetTop;
+  }
 });
+
+canvas.addEventListener("mousemove", draw);
 
 canvas.addEventListener("mouseup", function () {
   isDrawing = false;
+});
+
+document.addEventListener("keydown", handleTextInput);
+
+// Toggle button logic
+var toggleButton = document.getElementById("toggleButton");
+
+toggleButton.addEventListener("click", function () {
+  mode = mode === "draw" ? "text" : "draw";
+  toggleButton.textContent =
+    mode === "draw" ? "Switch to Text" : "Switch to Draw";
 });
 
 var videoStream = canvas.captureStream(30);
@@ -59,7 +94,7 @@ mediaRecorder.ondataavailable = function (e) {
 };
 
 mediaRecorder.start();
-setInterval(draw, 300);
+// setInterval(draw, 300);
 setTimeout(function () {
   mediaRecorder.stop();
 }, 5000);
